ceec4306ad021678364f00a5e683968fab87073d
maximys963 <maxim.p.jsdev@gmail.com>
2019-02-15 15:29:53 +0200

    Updates

---
+++ precache-manifest.0d501b4ade0c3bf1b6063220d039b9d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "971d10c6e0388dc95438c7369c8b0b7d",
+    "url": "/maximys963/IO-Technologies-test-task/static/media/right-arrow.971d10c6.svg"
+  },
+  {
+    "revision": "fd04e335eeeeb3e0d81572d25c50cc0d",
+    "url": "/maximys963/IO-Technologies-test-task/static/media/back.fd04e335.svg"
+  },
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/maximys963/IO-Technologies-test-task/static/media/3medal.98642d3c.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/maximys963/IO-Technologies-test-task/static/media/2medal.74629788.svg"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/maximys963/IO-Technologies-test-task/static/media/1medal.86c17a70.svg"
+  },
+  {
+    "revision": "4de539266ad7baa1c121",
+    "url": "/maximys963/IO-Technologies-test-task/static/js/runtime~main.4de53926.js"
+  },
+  {
+    "revision": "6d48cc3c92601edb2ca3",
+    "url": "/maximys963/IO-Technologies-test-task/static/js/main.6d48cc3c.chunk.js"
+  },
+  {
+    "revision": "d0383670088f42bfa617",
+    "url": "/maximys963/IO-Technologies-test-task/static/js/2.d0383670.chunk.js"
+  },
+  {
+    "revision": "6d48cc3c92601edb2ca3",
+    "url": "/maximys963/IO-Technologies-test-task/static/css/main.957aa34d.chunk.css"
+  },
+  {
+    "revision": "40c883538166d6fc1fb7b599b7bef57e",
+    "url": "/maximys963/IO-Technologies-test-task/index.html"
+  }
+];
